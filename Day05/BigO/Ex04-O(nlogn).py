6c9f0c1ce3b897e6f2160cebe9f22929133df8e9
KoreaIT <test@koreaid.com>
2023-04-27 21:41:02 +0900

    BigO

--- Day05/BigO/Ex04-O(nlogn).py
+++ Day05/BigO/Ex04-O(nlogn).py
@@ -6,8 +6,77 @@
 def merge_sort(arr):
   # 리스트의 길이가 1이하이면 정렬이
   # 이미 끝난것으로 그대로 반환
+
   if len(arr) <= 1:
     return arr
+  
+  '''
+  arr = [5, 2, 8, 6, 1, 9, 3, 7] 
+  mid = 4
+  
+  (1) -> left = [2, 5, 6, 8]
+  left = merge_sort([5,2,8,6])
+  arr = [5,2,8,6]
+  mid = 2
+      (1-1) -> left = [2, 5]
+      left = merge_sort([5,2]) 
+      arr = [5,2]
+      mid = 1
+        (1-1-1) -> left = 5
+        left = merge_sort([5])
+        arr = [5]
+        (1-1-2) -> right = 2
+        right = merge_sort([2])
+        arr = [2]
+        merge([5], [2]) -> result = [2, 5]
+      (1-2) -> right = [6, 8]
+      right = merge_sort([8,6]) 
+      arr = [8,6]
+      mid = 1
+        (1-2-1) -> left = 8
+        left = merge_sort([8])
+        arr = [8]
+        (1-2-2) -> right = 6
+        left = merge_sort([6])
+        arr = [6]
+        merge([8], [6]) -> result = [6, 8]
+      
+      merge([2, 5], [6, 8]) -> result = [2, 5, 6, 8]
+  
+  (2)  right = [1, 3, 7, 9]   
+  right = merge_sort([1,9,3,7])
+  arr = [1,9,3,7]
+  mid = 2
+    (2-1) left = [1,9]
+    left = merge_sort([1,9])
+    arr = [1,9]
+    mid = 1
+      (2-1-1)
+      left = merge_sort([1])
+      arr = [1]
+      (2-1-2)
+      right = merge_sort([9])
+      arr = [9]
+      merge([1], [9]) -> result = [1, 9]
+    
+    (2-2) right = [3,7]
+    left = merge_sort([3,7])
+    arr = [3,7]
+    mid = 1
+      (2-2-1)
+      left = merge_sort([3])
+      arr = [3]
+      (2-2-2)
+      right = merge_sort([7])
+      arr = [7]
+      merge([3], [7]) -> result = [3, 7]
+      
+    merge([1,9], [3,7]) -> result = [1, 3, 7, 9]   
+  
+  merge([2, 5, 6, 8], [1, 3, 7, 9]) -> result = [1, 2, 3, 5, 6, 7, 8, 9]
+  
+
+  '''
   
   # 리스트 중간 인덱스 계산
   mid = len(arr) // 2
@@ -19,6 +88,12 @@ def merge_sort(arr):
   # 두 개의 리스트를 병합하여 정렬된 리스트 생성
   return merge(left, right)
 
+'''
+left = [2, 5]
+right = [6, 8]
+
+result = [2, 5, 6, 8]
+'''
 def merge(left, right):
   # 결과를 저장할 리스트 생성
   result = []
@@ -27,6 +102,24 @@ def merge(left, right):
   # 작은 값을 결과 리스트에 추가
   i = j = 0
   while i < len(left) and j < len(right):
+    '''
+    [2, 5], [6, 8]
+    i = 0
+    j = 0
+    [2] < [6]
+    
+    i = 1
+    j = 0
+    [5] < [6]
+    
+    
+    i = 2
+    j = 0
+   
+    result += left[2:]
+    result += right[0:]
+    '''
+    
     if left[i] < right[j]:
       result.append(left[i])
       i += 1
@@ -44,8 +137,5 @@ def merge(left, right):
 # 실행코드
 arr = [5, 2, 8, 6, 1, 9, 3, 7] 
 sorted_arr = merge_sort(arr)
-print(sorted_arr)
+print(sorted_arr) # [1, 2, 3, 5, 6, 7, 8, 9]
 
-  
-  
-  
