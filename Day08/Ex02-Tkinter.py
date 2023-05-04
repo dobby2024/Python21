5c5880ab59c261b268774349639f5a228f76df65
KoreaIT <test@koreaid.com>
2023-05-04 20:52:30 +0900

    tkinter sql

---
+++ Day08/Ex02-Tkinter.py
@@ -0,0 +1,19 @@
+'''
+파일명: Ex02-Tkinter.py
+'''
+from tkinter import *
+import time
+
+tk = Tk()
+canvas = Canvas(tk, width=500, height=500)
+canvas.pack()
+# 다각형
+canvas.create_polygon(250, 400, 275, 450, 225, 450)
+canvas.create_polygon(250, 400, 275, 450, 225, 450)
+
+for i in range(0, 70):
+  canvas.move(1, -5, -5)
+  canvas.move(2, 5, -5)
+  
+  tk.update()
+  time.sleep(0.05)  
